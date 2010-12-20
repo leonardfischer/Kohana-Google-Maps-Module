b33b0cecc94524db5429e75f18ecf93ee1bd6917
Leonard Fischer <solidsnakedx@web.de>
2010-12-20 13:52:17 +0100

    Updated the config with new values and descriptions

--- config/gmap.php
+++ config/gmap.php
@@ -1,10 +1,21 @@
 <?php defined('SYSPATH') or die('No direct access allowed.');
 
 return array (
+	// Default map-center.
 	'default_lat' => 51.1731,
 	'default_lng' => 6.8328,
+
+	// Default zoom-level.
 	'default_zoom' => 12,
-	'default_sensor' => false, // Used for mobile devices.
+
+	 // Default "sensor" setting - Used for mobile devices.
+	'default_sensor' => false,
+
+	// Default map-type.
 	'default_maptype' => 'road',
+
+	// View
 	'default_view' => 'gmap',
+	'gmap_size_x' => '100%',
+	'gmap_size_y' => '100%',
 );
